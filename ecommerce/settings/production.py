2f7aaffb5f88089102dba475197e83c08f131a34
kadensungbincho <kadensungbincho@gmail.com>
2018-09-30 22:00:55 +0900

    Add whitenoise in production.py

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 LOGOUT_REDIRECT_URL = '/login/'
 ROOT_URLCONF = 'ecommerce.urls'
