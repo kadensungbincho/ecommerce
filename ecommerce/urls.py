e41beafc7ee6fd212f603b8861f6967c81bde9dc
kadensungbincho <kadensungbincho@gmail.com>
2018-10-07 23:21:28 +0900

    futher on digital products

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -26,6 +26,7 @@ from addresses.views import checkout_address_create_view, checkout_address_reuse
 from billing.views import payment_method_view, payment_method_create_view
 from carts.views import cart_detail_api_view
 from marketing.views import MarketingPreferenceUpdateView, MailchimpWebhookView
+from orders.views import LibraryView
 
 from .views import home_page, about_page, contact_page
 
@@ -49,6 +50,7 @@ urlpatterns = [
     url(r'^billing/payment-method/create/$', payment_method_create_view, name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^library/$', LibraryView.as_view(), name='library'),
     url(r'^orders/', include(("orders.urls", "orders"), namespace='orders')),
     url(r'^products/', include(("products.urls", "products"), namespace='products')),
     url(r'^search/', include(("search.urls", "search"), namespace='search')),
--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Order
+from .models import Order, ProductPurchase
 
 admin.site.register(Order)
+
+admin.site.register(ProductPurchase)
